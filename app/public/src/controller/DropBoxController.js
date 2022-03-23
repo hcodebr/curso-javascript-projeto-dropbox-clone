02aefca701c0856aec260faea6abbe8d5f78472d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-03-22 23:01:23 -0300

    tinhamos um seletor de arquivos oculto, entao quando o click no enviar é feito ele força o click nesse seletor

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,10 +1,13 @@
 class DropBoxController {
 
     constructor(){
+        this.inputFilesEl = document.querySelector('#files')
         this.btnSendFileEl = document.querySelector('#btn-send-file')
         this.InitEvents();
     }
     InitEvents(){
-
+        this.btnSendFileEl.addEventListener('click', e=>{
+            this.inputFilesEl.click();
+        });
     }
 }   
