088190debf45d7ceea8d661d43e776092364d54d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-03-22 23:06:20 -0300

    Adicionando a barrinha de progresso

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,13 +1,19 @@
 class DropBoxController {
 
     constructor(){
-        this.inputFilesEl = document.querySelector('#files')
-        this.btnSendFileEl = document.querySelector('#btn-send-file')
+        this.inputFilesEl = document.querySelector('#files') //seletor de arquivos
+        this.btnSendFileEl = document.querySelector('#btn-send-file') //botao de enviar arquivos
+        this.snackModalEl = document.querySelector('#react-snackbar-root') //barra de progresso
         this.InitEvents();
     }
     InitEvents(){
         this.btnSendFileEl.addEventListener('click', e=>{
             this.inputFilesEl.click();
+
         });
+        this.inputFilesEl.addEventListener('change', e=>{
+            console.log(e.target.files);
+            this.snackModalEl.style.display = "block"
+        })
     }
 }   
