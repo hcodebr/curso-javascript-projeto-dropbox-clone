daccea2d82542c40b3c1160b677c2a403c64ed62
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-03-29 22:18:11 -0300

    documentando o metodo de upload de arquivos

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -19,11 +19,27 @@ class DropBoxController {
     }
     uploadTask(files){
         let promises = []; //usamos uma promise pois cada arquivo pode ocorrer o upload ou falhar
-        [...files].forEach(file=>{
-            promises.push(new Promise((resolve, reject)=>[
+        [...files].forEach(files=>{ //criamos um spread para 
+            promises.push(new Promise((resolve, reject)=>{ //pega o array vazio e da push nas promises
+                let ajax = new XMLHttpRequest(); //aqui criamos um XML request que vai ser enviado como ajax(JOSn)
+                ajax.open('POST', '/upload') //Abrimos a conexãop ajax e passamos a rota upload e o método post
+                ajax.onload = event => { //assim que o ajax é carregado ele executa o try catch
+                    try {
+                        resolve(JSON.parse(ajax.responseText)) //o resolve da promise é o texto do ajax(JSOn)
+                    } catch (e) {
+                        reject(e); //erro
+                    }
+                };
+                ajax.onerror = event => {
+                    reject(event) //Retorna o erro
+                }
 
-            ]));
-        }) //convertemos em array e usamos o spread 
-        return Promise.all(promises) //redebe todas as promises
+                let formData = new FormData(); //criamos um form data
+                formData.append('input-file', files )   //recebe os files(array recebendo as promises)
+                ajax.send(formData); //O form data salva o arquivo recebido  pelo ajax e envia
+
+            }));
+        }); //convertemos em array e usamos o spread 
+        return Promise.all(promises) //redebe todas as promises e faz o controle doque deu resolve ou reject
     }
 }   
