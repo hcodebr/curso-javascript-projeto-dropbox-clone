959b618b0324248ab4d5b0a686e46122f99e6953
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-03-29 22:44:46 -0300

    criando a rota do upload e isntalando o formidable

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable'); //instalando o formidable
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/upload', (req, res)=>{ //criando a rota upload
+
+    res.json(req.body) //ela recebe o JSON
+
+})
 module.exports = router;
