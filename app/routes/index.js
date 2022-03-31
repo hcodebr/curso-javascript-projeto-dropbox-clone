cfb4206b3cf00f31dfbfbd1466f4deadef2d673e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-03-30 21:10:32 -0300

    Aplicando o formidable no projeto, que no caso é uma depencia boa para tratar arquivos..

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/upload', (req, res)=>{ //criando a rota upload
+  let form = new formidable.IncomingForm({ //envia o formulario com o diretorio da pasta upload que esta na raiz
+    keepExtensions: true, //mantem as extensões do arquivo
+    uploadDir: './upload' //envia o diretório 
+  })
+
+    form.parse(req, (err, fields, files)=>{ //pega o arquivo e da um parse para interpretar
+      //recebendo o req(JSON), e na arrow function ele recebe o erro(caso haja), 
+      //o fields e o files separa os dados enviados via post para verificar oque é arquivo e oque nao é
+      //colocando em 2 JSONS diferentes
+      
+    })
 
     res.json(req.body) //ela recebe o JSON
 
