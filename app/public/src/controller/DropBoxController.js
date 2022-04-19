553c1d89e2a0bfc0a3d2bb73af572161c81d6341
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-04-19 15:50:16 -0300

    criando mais um case para webp

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -284,7 +284,7 @@ class DropBoxController {
       case 'image/png':
       case 'image/jpg':
       case 'image/gif':
-      
+      case 'image/webp':
       return `
             <svg version="1.1" id="Camada_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="160px" height="160px" viewBox="0 0 160 160" enable-background="new 0 0 160 160" xml:space="preserve">
             <filter height="102%" width="101.4%" id="mc-content-unknown-large-a" filterUnits="objectBoundingBox" y="-.5%" x="-.7%">
