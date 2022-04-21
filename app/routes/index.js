1107abe3c1b5586b79a314265d6b50e82f076fe0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-04-20 23:16:36 -0300

    criando a rota e passando o método delete

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.delete('/file', (req, res)=>{
+  let form = new formidable.IncomingForm({ //envia o formulario com o diretorio da pasta upload que esta na raiz
+    uploadDir: './upload', //envia o diretório 
+    keepExtensions: true //mantem as extensões do arquivo
+  })
+
+    form.parse(req, (err, fields, files)=>{ //pega o arquivo e da um parse para interpretar
+      //recebendo o req(JSON), e na arrow function ele recebe o erro(caso haja), 
+      //o fields e o files separa os dados enviados via post para verificar oque é arquivo e oque nao é
+      //colocando em 2 JSONS diferentes
+      res.json({
+        files
+      })
+    })
+
+});
 router.post('/upload', (req, res) =>{ //criando a rota upload
   let form = new formidable.IncomingForm({ //envia o formulario com o diretorio da pasta upload que esta na raiz
     uploadDir: './upload', //envia o diretório 
